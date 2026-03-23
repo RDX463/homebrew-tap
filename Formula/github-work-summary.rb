b2e4567d2a59771a43d19e335dbe8c4ea7aff8ec
RDX463 <balsarafrohan627@gmail.com>
2026-03-23 18:29:26 +0530

    github-work-summary 0.1.8

--- Formula/github-work-summary.rb
+++ Formula/github-work-summary.rb
@@ -1,24 +1,24 @@
 class GithubWorkSummary < Formula
   desc "Summarize your GitHub work from the last 24 hours"
   homepage "https://github.com/RDX463/github-work-summary"
-  version "0.1.7"
+  version "0.1.8"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/RDX463/github-work-summary/releases/download/v0.1.7/github-work-summary-darwin-arm64.tar.gz"
-      sha256 "4f5bd8fdd4cee3a10869e00a408ff515e39ab50d825a334528cf3bd3d80455b7"
+      url "https://github.com/RDX463/github-work-summary/releases/download/v0.1.8/github-work-summary-darwin-arm64.tar.gz"
+      sha256 "734d721bafaf2d7ee7414e7ecb90149004c130aaa8eeec13ed99f24383e7c4fe"
     else
-      url "https://github.com/RDX463/github-work-summary/releases/download/v0.1.7/github-work-summary-darwin-amd64.tar.gz"
-      sha256 "b795dcdb09e9da304dd7ac23630370e98497fda456e8c1181247d8239438a859"
+      url "https://github.com/RDX463/github-work-summary/releases/download/v0.1.8/github-work-summary-darwin-amd64.tar.gz"
+      sha256 "987b00e3889e85b6da90f0b738a570ab88e85139124ff546b75acbb0c0ee2b91"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/RDX463/github-work-summary/releases/download/v0.1.7/github-work-summary-linux-arm64.tar.gz"
-      sha256 "b5caae03e146cc314722c7a6348cde55ba3b9ad8c714fae13577e2eabab5325a"
+      url "https://github.com/RDX463/github-work-summary/releases/download/v0.1.8/github-work-summary-linux-arm64.tar.gz"
+      sha256 "9fabde1f79feb537b85dd23f77e2ab5676472f47ab77dd17a271d52cbd35fc91"
     else
-      url "https://github.com/RDX463/github-work-summary/releases/download/v0.1.7/github-work-summary-linux-amd64.tar.gz"
-      sha256 "eac05d07ae55e36d74a70e28196e7c2998fccb636cfdb4e793c0dfed8948242d"
+      url "https://github.com/RDX463/github-work-summary/releases/download/v0.1.8/github-work-summary-linux-amd64.tar.gz"
+      sha256 "74fbb29feba99d7999783ce9cd0390e38dd1eee01fa76481e963bf8e291ed2fe"
     end
   end
 
