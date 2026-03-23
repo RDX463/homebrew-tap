f1aa35f9114658fd82ddc319d054a589f38b1d79
RDX463 <balsarafrohan627@gmail.com>
2026-03-23 13:44:53 +0530

    Add github-work-summary formula

---
+++ Formula/github-work-summary.rb
@@ -0,0 +1,15 @@
+class GithubWorkSummary < Formula
+  desc "Summarize your GitHub work from the last 24 hours"
+  homepage "https://github.com/RDX463/github-work-summary"
+  url "https://github.com/RDX463/github-work-summary/releases/download/v0.1.0/github-work-summary-darwin-arm64.tar.gz"
+  sha256 "7eee8d5b8b972223e987e47f5d1c41c3d6f14bdc1d763cd241115854a6c30eb0"
+  version "0.1.0"
+
+  def install
+    bin.install "github-work-summary"
+  end
+
+  test do
+    assert_match "Summarize your GitHub work", shell_output("#{bin}/github-work-summary --help")
+  end
+end
