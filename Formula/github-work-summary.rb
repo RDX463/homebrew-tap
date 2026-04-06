843e17354cbe8648a621ec3407934a69d3d8e060
RDX463 <balsarafrohan627@gmail.com>
2026-04-06 23:03:32 +0530

    github-work-summary v2.2.1

--- Formula/github-work-summary.rb
+++ Formula/github-work-summary.rb
@@ -1,9 +1,9 @@
 class GithubWorkSummary < Formula
   desc "Summarize your GitHub work from the last 24 hours"
   homepage "https://github.com/RDX463/github-work-summary"
-  url "https://github.com/RDX463/github-work-summary/archive/refs/tags/v2.2.0.tar.gz"
-  sha256 "17ac985d0f3cf501e0e92772d8d7ee2896b2314e8e4a9122d466126b8ec01ec2"
-  version "2.2.0"
+  url "https://github.com/RDX463/github-work-summary/archive/refs/tags/v2.2.1.tar.gz"
+  sha256 "8bb4a467030fe0235aaed2ef18f76356efe3fd5e78818d2b85abc010e2b8771b"
+  version "2.2.1"
 
   depends_on "go" => :build
 
