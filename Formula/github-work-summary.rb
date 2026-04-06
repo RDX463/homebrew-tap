4c061c2244b99fdc341720784c05d5ec59be0805
RDX463 <balsarafrohan627@gmail.com>
2026-04-06 20:11:37 +0530

    Update github-work-summary to v1.5.0

--- Formula/github-work-summary.rb
+++ Formula/github-work-summary.rb
@@ -1,11 +1,14 @@
 class GithubWorkSummary < Formula
   desc "Summarize your GitHub work from the last 24 hours"
   homepage "https://github.com/RDX463/github-work-summary"
-  url "https://github.com/RDX463/github-work-summary/releases/download/v1.0.0/github-work-summary-darwin-arm64.tar.gz"
-  sha256 "a44c462d668b1a20972f180fc6e2e6ef93f0fb58f8bb559b6a91253fe9e46637"
-  version "1.0.0"
+  url "https://github.com/RDX463/github-work-summary/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "85b3ecb0acc73fad6604ce57606dc537758ac363a00fe698928b00c6b0086ed6"
+  version "1.5.0"
+
+  depends_on "go" => :build
 
   def install
+    system "go", "build", "-o", "github-work-summary", "main.go"
     bin.install "github-work-summary"
     bin.install_symlink "github-work-summary" => "gws"
   end
